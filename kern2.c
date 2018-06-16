b1a806e5f6126b37d39eddb9ee933434bfe78cdd
emariotti3 <eugeniamariotti93@gmail.com>
2018-06-15 22:51:54 -0300

    Added code to permit call to two_stacks:performs two calls vga_write on different stack spaces.

--- kern2.c
+++ kern2.c
@@ -2,36 +2,11 @@
 #include "multiboot.h"
 #include "lib/string.h"
 
-#define BUF_SZ 125
-#define SHIFT_MIB 11
-
-char mem[256] = "Physical memory: ";
-char tmp[64] = "";
-
-
 
 void kmain(const multiboot_info_t *mbi) {
     vga_write("kern2 loading.............", 8, 0x70);
-    if (mbi->flags) {
-        char buf[BUF_SZ] = "cmdline: ";
-        char *cmdline = (void *) mbi->cmdline;
-        strlcat(buf, cmdline, BUF_SZ);
-        vga_write(buf, 9, 0x07);
-    }
-    //if (mbi->flags & MEM_PRESENT) { TODO: chequear los flags
-    if (fmt_int((mbi->mem_lower + mbi->mem_upper) >> 10 , tmp, sizeof tmp)) {
-        strlcat(mem, tmp, sizeof mem);
-        strlcat(mem, "MiB total", sizeof mem);
-    }
-    if (fmt_int(mbi->mem_lower, tmp, sizeof tmp)) {
-        strlcat(mem, "(", sizeof mem);
-        strlcat(mem, tmp, sizeof mem);
-        strlcat(mem, "KiB base, ", sizeof mem);
-    }
-    if (fmt_int(mbi->mem_upper, tmp, sizeof tmp)) {
-        strlcat(mem, tmp, sizeof mem);
-        strlcat(mem, "KiB extended).", sizeof mem);
-    }
-    vga_write(mem, 10, 0x07);
 
+    // A remplazar por una llamada a two_stacks(),
+    // definida en stacks.S.
+    asm("call two_stacks");
 }
